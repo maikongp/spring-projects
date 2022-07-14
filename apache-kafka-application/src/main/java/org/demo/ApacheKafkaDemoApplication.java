73aae7bce09863a544b3c2b0843612d9a5179c35
maikongp <maikongp@hotmail.com>
2022-07-14 16:44:13 -0300

    Implemented the Consumer Kakfa Message

--- apache-kafka-application/src/main/java/org/demo/ApacheKafkaDemoApplication.java
+++ apache-kafka-application/src/main/java/org/demo/ApacheKafkaDemoApplication.java
@@ -16,7 +16,8 @@ public class ApacheKafkaDemoApplication {
     @Bean
     CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate){
         return args -> {
-            kafkaTemplate.send("amigoscode", "hello kafka");
+            for (int i=0; i < 100; i++)
+            kafkaTemplate.send("amigoscode", "hello kafka :)" + i);
         };
     }
 }
